6d1540fc33c920abd99dce104f60daa4e808775e
rit <coding3392@gmail.com>
2018-05-25 20:44:06 +0900

    덧셈까지

---
+++ 소스.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+
+	char c;
+
+	cin >> a >> c >> b;
+
+	int result;
+
+	while (c != '.') {
+		switch (c) {
+		case '+':
+			result = a + b;
+			break;
+		case '-':
+			result = a - b;
+			break;
+		default:
+			break;
+		}
+		cout << result << endl;
+		cin >> a >> c >> b;
+
+	}
+	return 0;
+}
